a9865bfb2c3a14374d7ac12b3b9b2357bef25bd8
Dawood Iftakhar <dawood.iftakhar87@gmail.com>
2025-05-11 14:01:36 +0500

    Add files via upload

---
+++ list_dictionary_practice.py
@@ -0,0 +1,17 @@
+# Part 1: List Practice 
+fruits=["mango", "plum", "orange"]
+print("furitlist:", fruits)
+print("secondfruit:",fruits[1])
+fruits.append("banana")
+print("updatefruitlist:", fruits)
+
+# Part 2: Dictionary - Studnet Information
+student= {
+    "name": "Dawood",
+    "age": 37,
+    "grade": "A"
+}
+print("studentinfo:", student)
+print("Name", student["name"])
+student["College"]="Sir Syed College"
+print("update studentinfo:", student)
